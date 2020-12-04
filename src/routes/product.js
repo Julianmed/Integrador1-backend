6937bc4292bbaca240a3352b05318b59f3b9858e
Julian Angel Gallego <julian.angelg@udea.edu.co>
2020-12-04 00:54:47 -0500

    product ajuste

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../database');
 
-router.get('/', async (req,res)=>{
-    const productos_servicios = await pool.query('SELECT * FROM producto_servicio');
+router.get('/all/:id_concepto', async (req,res)=>{
+    const productos_servicios = await pool.query('SELECT * FROM WHERE id_concepto= ?',[req.params.id_concepto]);
     res.json(productos_servicios);
 });
 
