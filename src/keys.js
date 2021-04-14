2bfeef2f9f6109b9f3f06cd9d7279b5c4a358b4d
Julian Angel Gallego <julian.angelg@udea.edu.co>
2021-04-14 09:39:56 -0500

    update data of database

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,14 @@
 module.exports = {
+    // database: {
+    //     host: 'localhost',
+    //     user: 'root',
+    //     password: 'root',
+    //     database: 'CONTROL_GASTOS'
+    // }
     database: {
-        host: 'localhost',
-        user: 'root',
-        password: 'root',
-        database: 'CONTROL_GASTOS'
+        host: 'grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user: 'cdr2fpq47zp67b41',
+        password: 'oroujtvb9sx8hyy9',
+        database: 'pigqdbz7fuxsszg8'
     }
 }
